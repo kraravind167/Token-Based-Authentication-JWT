30697cd7f6670ab5dac2646f367fbcf48f01a978
kraravind <aravindkr86@gmail.com>
2018-11-19 17:41:00 +0530

    Added Protected routes '/auth/getAll'
    only If token is provided in header under field 'access-token', user can able to access the proteceted routes.
    here under '/auth' all are protected routes.

--- server.js
+++ server.js
@@ -4,21 +4,56 @@ const express = require('express'),
 const app = express();
 
 const user = require('./routes/user.route');
-
+const ProtectedRoute = express.Router();
+const jwt = require('jsonwebtoken');
+const User = require('./models/user.model')
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/jwttokenauth',{useNewUrlParser:true});
+mongoose.connect('mongodb://localhost:27017/jwttokenauth', { useNewUrlParser: true });
 
 app.get('/checking', (req, res) => {
     res.json({
-       "Message": "Function works"
+        "Message": "Function works"
     });
 });
 
-app.use('/user',user);
+app.use('/user', user);
+
+/* Protected Routes */
+app.use('/auth', ProtectedRoute);
+
+ProtectedRoute.use((req, res, next) => {
+    var token = req.headers['access-token'];
+
+    if (token) {
+        jwt.verify(token, 'secret', (err, decoded) => {
+            if (err) {
+                return res.json({ "msg": "Invalid Token" });
+            }
+            else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+    }
+    else {
+        res.send({
+            "msg": "No token provided"
+        });
+    }
+
+});
+
+ProtectedRoute.get('/getAll', (req, res) => {
+    User.find({}).then((user) => {
+        res.json(user);
+    });
+
+});
+
 
 app.listen(port, () => {
     console.log('Server is running on port' + port);
